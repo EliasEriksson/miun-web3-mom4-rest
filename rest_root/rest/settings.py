edd6e22d73d0da86d11b4b3eb1e6e1681fe1b168
EliasEriksson <EliasEriksson95@gmail.com>
2021-09-27 14:09:02 +0200

    desperate attempt to solve cors errors;

--- rest_root/rest/settings.py
+++ rest_root/rest/settings.py
@@ -60,6 +60,9 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    "*",
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
