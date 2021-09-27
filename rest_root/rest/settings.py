14bc9e1b0ac3fbb4549f4a794d98048ca4ed7a90
EliasEriksson <EliasEriksson95@gmail.com>
2021-09-27 13:40:35 +0200

    allowed hosts set to *

--- rest_root/rest/settings.py
+++ rest_root/rest/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = credentials["secret_key"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "*"
+]
 
 # Application definition
 
