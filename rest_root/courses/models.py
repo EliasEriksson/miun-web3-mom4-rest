68e7928d7e131b88418639193690ee593bfcf630
EliasEriksson <EliasEriksson95@gmail.com>
2021-10-01 12:42:19 +0200

    added extra validators on model fields.

--- rest_root/courses/models.py
+++ rest_root/courses/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import RegexValidator
 from django.utils.html import strip_tags
 
 
@@ -11,9 +12,13 @@ class Course(models.Model):
     This class can then be used as a way to make queries to the database and get objects back with the classes
     attributes / methods i.e: Course.objects.all() would get all the courses in the database.
     """
-    code = models.CharField(max_length=6, primary_key=True)
+    code = models.CharField(max_length=6, primary_key=True, validators=[
+        RegexValidator(r"^(?:\w|\d)+$", "Can only contain letters and number.")
+    ])
     name = models.CharField(max_length=128)
-    progression = models.CharField(max_length=1)
+    progression = models.CharField(max_length=1, validators=[
+        RegexValidator(r"^(?:\w)$", "Can only contain letters.")
+    ])
     plan = models.CharField(max_length=255)
 
     def __str__(self):
@@ -26,8 +31,8 @@ class Course(models.Model):
         before saving the fields are stripped from html tags to prevent XXS
         and other not so nice things.
         """
-        self.code = strip_tags(self.code)
-        self.name = strip_tags(self.name)
-        self.progression = strip_tags(self.progression)
+        self.code = strip_tags(self.code).upper()
+        self.name = strip_tags(self.name).capitalize()
+        self.progression = strip_tags(self.progression).upper()
         self.plan = strip_tags(self.plan)
         return super().save(*args, **kwargs)
